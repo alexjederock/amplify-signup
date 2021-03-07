14e55bb7be045fc26f9d86e53247f9aa8726ed22
Alex Jederlinic <alex@jederock.com>
2021-03-07 09:55:55 -0500

    react app with authorization provided by amplify.

--- amplify-signup/src/App.js
+++ amplify-signup/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Amplify from 'aws-amplify'
-import awsconfig from './exports'
+import awsconfig from './aws-exports'
 import {AmplifySignOut, withAuthenticator} from '@aws-amplify/ui-react'
 
 Amplify.configure(awsconfig)
